be9aa6e3b017994401d0a6740b4791fba7727631
Carlos <carlosmoracla@gmail.com>
2020-03-21 20:34:38 +0100

    Simple implementation of the chatbot (WIP)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
-chat_bot = ChatBot(name='Jarvis', read_only=True,
+chat_bot = ChatBot(name='Carvis', read_only=True,
                    logic_adapters=['chatterbot.logic.MathematicalEvaluation',
                                    'chatterbot.logic.BestMatch'])
 
@@ -19,16 +19,18 @@ small_talk = ['hi there!',
               'i feel awesome',
               'excellent, glad to hear that.',
               'not so good',
-              'sorry to hear that.',
+              'ok boomer.',
               'what\'s your name?',
-              'i\'m pybot. ask me a math question, please.']
+              'i\'m Carvis. Boomer']
+math_talk_1 = ['pythagorean theorem',
+               'a squared plus b squared equals c squared.']
+math_talk_2 = ['law of cosines',
+               'c**2 = a**2 + b**2 - 2 * a * b * cos(gamma)']
 
 list_trainer = ListTrainer(chat_bot)
-for item in small_talk:
+for item in (small_talk, math_talk_1, math_talk_2):
     list_trainer.train(item)
 
 while 1:
     nb = input('You: ')
-    print("Bot: " + chat_bot.get_response(nb))
-
-
+    print(chat_bot.get_response(nb))
