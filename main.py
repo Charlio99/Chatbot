3d91f88a7aaa2e6e938fb2330b094a02d7bf0b72
Carlos Mora Clavero <carlosmoracla@gmail.com>
2020-05-31 15:15:11 +0200

    refactor(#5): main.py cleanup

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from Graph.readGraph import Decision
 from commands import Commands
 from singletonBot import Bot
 
-knownUsers = []  # todo: save these in a file,
-userStep = {}  # so they won't reset every time the bot restarts
-
 graph = Decision()
 graph.read_json()
 
